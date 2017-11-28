5119f9d6c3607011ba4b745c7a61de2289745909
tran-legacy <tran.le057@yahoo.com>
2017-11-28 11:27:10 -0800

    Updated

--- sortStatistics.cpp
+++ sortStatistics.cpp
@@ -13,8 +13,12 @@
 #include <fstream>
 #include <cstring>
 using namespace std; 
-void outputD() {
-
+void outputD(ArrayList<int>& list) {
+	ArrayList<int> selectionList = list; 
+	cout << "Filename: " << fileName << endl; 
+	cout << "Number of items: " << list.getSize(); 
+	cout <<	"Selection Sort Results:" << endl; 
+	list.
 }
 
 void output(char* fileName, ArrayList<int>& list) {
@@ -23,21 +27,33 @@ void output(char* fileName, ArrayList<int>& list) {
 	
 }
 
-void selectionSort() {
+void stats() {
 
 }
 
-void mergeSort() {
+void selectionSort(ArrayList<int>& list) {
 
 }
 
-void quickSort() {
+void mergeSort(ArrayList<int>& list) {
+
+}
+
+void quickSort(ArrayList<int>& list) {
 
 }
 
 int main(int argc, char* *argv) {
-	std::ifstream infile(arg[2]); 
-	if (strcmp[argv[2], "-d"] == 0 ) { // If the second word is -d
+	if (strcmp[argv[1], "-d"] == 0 ) { // If the second word is -d
+		ArrayList<int> originalArray; 	
+		std::ifstream infile(argv[2]); 
+		if (infile.is_open() && infile.good()) {
+			// Get numbers from list to the array
+			int num; 
+			while (infile >> num) {
+				originalArray.append(num); 
+			}
+		}
 		//IMPLEMENT THE THING
 	} else if (
 	
